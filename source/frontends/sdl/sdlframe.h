936c6752bd61d435ec2941c60c3b25a02908296f
sh95014 <95387068+sh95014@users.noreply.github.com>
2023-12-24 13:43:04 -0800

    remove #ifdef MARIANI

--- source/frontends/sdl/sdlframe.h
+++ source/frontends/sdl/sdlframe.h
@@ -4,9 +4,7 @@
 #include "frontends/common2/controllerquit.h"
 #include "frontends/common2/programoptions.h"
 #include "linux/network/portfwds.h"
-#ifndef MARIANI
 #include <SDL.h>
-#endif
 
 namespace sa2
 {
@@ -21,18 +19,14 @@ namespace sa2
 
     void FrameRefreshStatus(int drawflags) override;
     int FrameMessageBox(LPCSTR lpText, LPCSTR lpCaption, UINT uType) override;
-#ifndef MARIANI
     void GetBitmap(LPCSTR lpBitmapName, LONG cb, LPVOID lpvBits) override;
-#endif
     std::shared_ptr<NetworkBackend> CreateNetworkBackend(const std::string & interfaceName) override;
 
     void ProcessEvents(bool &quit);
 
     void FrameResetMachineState();
 
-#ifndef MARIANI
     const std::shared_ptr<SDL_Window> & GetWindow() const;
-#endif
 
     void getDragDropSlotAndDrive(size_t & slot, size_t & drive) const;
     void setDragDropSlotAndDrive(const size_t slot, const size_t drive);
@@ -46,7 +40,6 @@ namespace sa2
   protected:
     void SetApplicationIcon();
 
-#ifndef MARIANI
     virtual void ProcessSingleEvent(const SDL_Event & event, bool & quit);
     virtual void GetRelativeMousePosition(const SDL_MouseMotionEvent & motion, double & x, double & y) const = 0;
 
@@ -56,14 +49,11 @@ namespace sa2
     void ProcessDropEvent(const SDL_DropEvent & drop);
     void ProcessMouseButton(const SDL_MouseButtonEvent & button);
     void ProcessMouseMotion(const SDL_MouseMotionEvent & motion);
-#endif // MARIANI
 
     void SetFullSpeed(const bool value) override;
     bool CanDoFullSpeed() override;
 
-#ifndef MARIANI
     common2::Geometry getGeometryOrDefault(const std::optional<common2::Geometry> & geometry) const;
-#endif
 
     static double GetRelativePosition(const int value, const int width);
 
@@ -80,9 +70,7 @@ namespace sa2
 
     std::vector<PortFwd> myPortFwds;
 
-#ifndef MARIANI
     std::shared_ptr<SDL_Window> myWindow;
-#endif
 
     common2::ControllerQuit myControllerQuit;
   };
